9e378906aa6e77518c23c63485c066cbe658f47f
Karina <karina.kvych@gmail.com>
2023-09-07 15:28:41 +0100

    Styled nav bar

--- src/Navigation/Navbar.tsx
+++ src/Navigation/Navbar.tsx
@@ -1,68 +1,36 @@
-import React from "react"
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import EmergeMenu from "./EmergeMenu";
+import './navigation.css'
+import "../fonts/airbnb-cereal-app-bold.ttf"
 
-export default function Navbar(){
- return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
-      Navbar
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
 
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">
-            Home <span className="sr-only">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Features
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Pricing
-          </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdownMenuLink"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown link
-          </a>
-          <div
-            className="dropdown-menu"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
- )
+export default function Header() {
+    const [expandMenu, setExpandedMenu] = useState(false)
+
+    const showMenu = () => { setExpandedMenu(!expandMenu) }
+
+    return (
+        <section className="wrapper">
+            <div className="navigation">
+                <div className="title">
+                    <section className="menuButton" onClick={showMenu}>
+                        <div className="bar1"></div>
+                        <div className="bar2"></div>
+                        <div className="bar3"></div>
+                    </section>
+                    <em className="siteName">Tasty&Zesty</em>
+                </div>
+                <div className="takeSpase"></div>
+                <div className="pintedInfo">
+                    <Link className="" to="/">Home</Link>
+                    <Link className="" to="/login">Login</Link>
+                </div>
+            </div>
+
+                <div >
+                    <EmergeMenu menu={expandMenu} />
+                </div>
+        </section >
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react"
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import store from "./app/store"
-
-
 import { Provider } from 'react-redux'
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
